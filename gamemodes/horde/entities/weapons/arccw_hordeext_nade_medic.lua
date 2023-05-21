717b89dd71b909ea0c26d30f5e70c5f8f7a82a66
passass <misha.dorenskiy@gmail.com>
2023-05-21 10:57:13 +1000

    bugfix

--- gamemodes/horde/entities/weapons/arccw_hordeext_nade_medic.lua
+++ gamemodes/horde/entities/weapons/arccw_hordeext_nade_medic.lua
@@ -1,8 +1,8 @@
 if not ArcCWInstalled then return end
 -- Referenced From GSO
-SWEP.Base = "arccw_horde_nade_medic"
+SWEP.Base = "arccw_nade_medic"
 if CLIENT then
-    SWEP.WepSelectIcon = surface.GetTextureID("arccw/weaponicons/arccw_horde_nade_medic")
+    SWEP.WepSelectIcon = surface.GetTextureID("arccw/weaponicons/arccw_nade_medic")
 end
 SWEP.Horde_MaxMags = 50
 SWEP.ShootEntity = "arccw_thr_hordeext_medicgrenade"
