f5e571b8dc3e1b0f1005298ab371388fcbc565e7
passass <misha.dorenskiy@gmail.com>
2024-11-27 08:59:28 +1000

    nnq11

--- gamemodes/horde/gamemode/custom/horde_ext.lua
+++ gamemodes/horde/gamemode/custom/horde_ext.lua
@@ -36,7 +36,7 @@ local function ExtInclude(name, dir)
 	local sep = string.Split(name, "_")
 	name = dir .. name
 
-	-- Determine where to load the files
+	-- Determine where to load the files7
 	if sep[1] == "sv" then
 		if SERVER then
 			NEW_include(name)
